de6c2009636fead271488c9e8927463419bda235
tarunj292 <tarunj292@gmail.com>
2024-02-14 12:27:21 +0530

    fixed myself

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Login from './pages/Login';
+
+function App() {
+  return (
+
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+      </Routes>
+    
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,12 @@
+
+
+
+const Login = () => {
+    return (
+        <div>
+            <h1>fhdfhfdhfdhgdfjhg</h1>
+        </div>
+    )
+}
+
+export default Login
