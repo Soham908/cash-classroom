1e3de4e499db9b2e9aaed46dbe8685dff8bbda65
Soham908 <sohampalekar908@gmail.com>
2024-02-21 12:56:14 +0530

    courses card added

--- frontend/src/actions/dataFetchActions.js
+++ frontend/src/actions/dataFetchActions.js
@@ -4,7 +4,7 @@ import { ENDPOINT_URL } from "./../constants/constants";
 export const fetchCardData = async () => {
     try{
       const response = await axios.get(`${ENDPOINT_URL}/data/get-course-data`);
-      console.log(response.data);
+      return response.data.courseData
     }
     catch(err){
   
