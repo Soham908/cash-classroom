0ed284e4a885a34b5b553523e67aa11429f1fbb1
jaswant-rajput <jaswant.s@somaiya.edu>
2024-03-09 14:00:17 +0530

    some designing on the blogs page

--- frontend/src/components/BlogCard/BlogCard.js
+++ frontend/src/components/BlogCard/BlogCard.js
@@ -1,20 +1,34 @@
+import styles from "./blogCard.module.css";
 
-import styles from "./blogCard.module.css"
+const BlogCard = ({ blog }) => {
+	console.log(blog);
+	return (
+		<div className={styles.container}>
+			<img className={styles.coverImg} src="/blog.avif" alt="blog title" />
+			<h2 className={styles.title}>{blog?.title}</h2>
+			<div className={styles.details}>
+				<div className={styles.dateContainer}>
+					<img
+						className={styles.calendar}
+						src="/calendar.png"
+						alt="calendar logo"
+					/>
+					<span className={styles.spanValue}>
+						{blog?.createdAt.slice(0, 10)}
+					</span>
+				</div>
 
-const BlogCard = ({blog})=> {
-    console.log(blog)
-    return (
-        <div className={styles.container}>
-            <img src="/blog.avif" /> 
-            <h1>{blog?.title}</h1>
-            <div className={styles.details}>
-                <span>{blog?.createdAt.slice(0,10)}</span>
-                <br/>
-                <span>{blog?.minRead} mins read</span>
-            </div>
-            <button>Read more</button>
-        </div>
-    )
-}
+				<div className={styles.minReadContainer}>
+					<img className={styles.clock} src="clock.png" alt="clock logo" />
+					<span className={styles.spanValue}>{blog?.minRead} mins read</span>
+				</div>
+			</div>
+			<div className={styles.navigateContainer}>
+				<span>Read More</span>
+				<img src="/up-right-arrow.png" alt="navigate logo" />
+			</div>
+		</div>
+	);
+};
 
-export default BlogCard
+export default BlogCard;
