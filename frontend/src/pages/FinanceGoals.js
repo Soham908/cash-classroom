e22af32a6f1f71df001085d22e853f72a82377f2
jaswant-rajput <jaswant.s@somaiya.edu>
2024-03-25 11:49:52 +0530

    imports fixed

--- frontend/src/pages/FinanceGoals.js
+++ frontend/src/pages/FinanceGoals.js
@@ -9,116 +9,117 @@ import CardContent from "@mui/joy/CardContent";
 import { CardActions, Grid, Input, LinearProgress, Snackbar } from "@mui/joy";
 import Typography from "@mui/joy/Typography";
 import { Box, TextField } from "@mui/material";
+import GoalComponent from "./../components/GoalComponent";
 
 const FinanceGoals = () => {
-  const user = useAuthStore.getState().user;
-  const [goalFormData, setGoalFormData] = useState({
-    name: "",
-    target: null,
-    currentAmount: null,
-  });
-  const [refresh, setRefresh] = useState(false);
-  const [goals, setGoals] = useState([]);
-  const [amountUpdate, setAmountUpdate] = useState(null);
-  const [snackBarOpen, setSnackBarOpen] = useState(false);
-  const [goalCompleted, setGoalCompleted] = useState(false)
+	const user = useAuthStore.getState().user;
+	const [goalFormData, setGoalFormData] = useState({
+		name: "",
+		target: null,
+		currentAmount: null,
+	});
+	const [refresh, setRefresh] = useState(false);
+	const [goals, setGoals] = useState([]);
+	const [amountUpdate, setAmountUpdate] = useState(null);
+	const [snackBarOpen, setSnackBarOpen] = useState(false);
+	const [goalCompleted, setGoalCompleted] = useState(false);
 
-  useEffect(() => {
-    const fetchGoals = async () => {
-      const response = await fetchGoalsById(user?.data?._id);
-      setGoals(response.goals);
-    };
-    fetchGoals();
-  }, [refresh]);
+	useEffect(() => {
+		const fetchGoals = async () => {
+			const response = await fetchGoalsById(user?.data?._id);
+			setGoals(response.goals);
+		};
+		fetchGoals();
+	}, [refresh]);
 
-  const handleGoalsFormChange = (e) => {
-    setGoalFormData((p) => ({ ...p, [e.target.name]: e.target.value }));
-  };
+	const handleGoalsFormChange = (e) => {
+		setGoalFormData((p) => ({ ...p, [e.target.name]: e.target.value }));
+	};
 
-  const handleCreateGoal = async () => {
-    const data = { ...goalFormData, userId: user?.data?._id };
-    const response = await createGoal(data);
-    if (response.success) {
-      setRefresh((p) => !p);
-    }
-  };
+	const handleCreateGoal = async () => {
+		const data = { ...goalFormData, userId: user?.data?._id };
+		const response = await createGoal(data);
+		if (response.success) {
+			setRefresh((p) => !p);
+		}
+	};
 
-  const handleSavedValue = async (data) => {
-    const check = checkGoalCompleted({
-      goalTarget: data.goalTarget,
-      goalCurrentAmt: data.goalCurrentAmt,
-    });
-    if (!check) {
-      var sendData = { id: data.id, updatedAmount: 0 };
-      if (
-        parseInt(data.goalCurrentAmt) + parseInt(data.updateAmount) >=
-        data.goalTarget
-      ) {
-        sendData.updatedAmount = data.goalTarget;
-      } else {
-        sendData.updatedAmount =
-          parseInt(data.goalCurrentAmt) + parseInt(data.updateAmount);
-        console.log(sendData.updatedAmount);
-      }
-      const response = await updateGoal({
-        id: sendData.id,
-        updatedAmount: sendData.updatedAmount,
-      });
-      console.log(response);
-      setRefresh((prev) => !prev);
-    }
-  };
+	const handleSavedValue = async (data) => {
+		const check = checkGoalCompleted({
+			goalTarget: data.goalTarget,
+			goalCurrentAmt: data.goalCurrentAmt,
+		});
+		if (!check) {
+			var sendData = { id: data.id, updatedAmount: 0 };
+			if (
+				parseInt(data.goalCurrentAmt) + parseInt(data.updateAmount) >=
+				data.goalTarget
+			) {
+				sendData.updatedAmount = data.goalTarget;
+			} else {
+				sendData.updatedAmount =
+					parseInt(data.goalCurrentAmt) + parseInt(data.updateAmount);
+				console.log(sendData.updatedAmount);
+			}
+			const response = await updateGoal({
+				id: sendData.id,
+				updatedAmount: sendData.updatedAmount,
+			});
+			console.log(response);
+			setRefresh((prev) => !prev);
+		}
+	};
 
-  const checkGoalCompleted = ({ goalTarget, goalCurrentAmt }) => {
-    if (goalTarget === goalCurrentAmt) {
-      console.log("goal complete");
-      setSnackBarOpen(true);
-      setGoalCompleted(true)
-      return true;
-    }
-  };
+	const checkGoalCompleted = ({ goalTarget, goalCurrentAmt }) => {
+		if (goalTarget === goalCurrentAmt) {
+			console.log("goal complete");
+			setSnackBarOpen(true);
+			setGoalCompleted(true);
+			return true;
+		}
+	};
 
-  return (
-    <ProtectRoutes>
-      <Grid container spacing={2} alignItems="center">
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="Name"
-            name="name"
-            value={goalFormData.name}
-            onChange={(e) => handleGoalsFormChange(e)}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="Target"
-            name="target"
-            type="number"
-            value={goalFormData.target}
-            onChange={(e) => handleGoalsFormChange(e)}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="Current Amount"
-            name="currentAmount"
-            type="number"
-            value={goalFormData.currentAmount}
-            onChange={(e) => handleGoalsFormChange(e)}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Button variant="solid" color="primary" onClick={handleCreateGoal}>
-            Create Goal
-          </Button>
-        </Grid>
-      </Grid>
+	return (
+		<ProtectRoutes>
+			<Grid container spacing={2} alignItems="center">
+				<Grid item xs={12}>
+					<TextField
+						fullWidth
+						label="Name"
+						name="name"
+						value={goalFormData.name}
+						onChange={(e) => handleGoalsFormChange(e)}
+					/>
+				</Grid>
+				<Grid item xs={12}>
+					<TextField
+						fullWidth
+						label="Target"
+						name="target"
+						type="number"
+						value={goalFormData.target}
+						onChange={(e) => handleGoalsFormChange(e)}
+					/>
+				</Grid>
+				<Grid item xs={12}>
+					<TextField
+						fullWidth
+						label="Current Amount"
+						name="currentAmount"
+						type="number"
+						value={goalFormData.currentAmount}
+						onChange={(e) => handleGoalsFormChange(e)}
+					/>
+				</Grid>
+				<Grid item xs={12}>
+					<Button variant="solid" color="primary" onClick={handleCreateGoal}>
+						Create Goal
+					</Button>
+				</Grid>
+			</Grid>
 
-      <Box sx={{ display: "flex", flexWrap: "wrap" }}>
-        {/* {goals.map((goalData, index) => {
+			<Box sx={{ display: "flex", flexWrap: "wrap" }}>
+				{/* {goals.map((goalData, index) => {
           return (
             <Card sx={{ margin: 2 }}>
               <AspectRatio ratio={2}>
@@ -193,24 +194,24 @@ const FinanceGoals = () => {
             </Card>
           );
         })} */}
-        {goals.map((goalData, index) => (
-        <GoalComponent
-          key={index}
-          goalData={goalData}
-          handleSavedValue={handleSavedValue}
-        />
-      ))}
-      </Box>
-      <Snackbar
-        anchorOrigin={{ vertical: "top", horizontal: "center" }}
-        open={snackBarOpen}
-        autoHideDuration={1500}
-        onClose={() => setSnackBarOpen(false)}
-      >
-        Goals is completed
-      </Snackbar>
-    </ProtectRoutes>
-  );
+				{goals.map((goalData, index) => (
+					<GoalComponent
+						key={index}
+						goalData={goalData}
+						handleSavedValue={handleSavedValue}
+					/>
+				))}
+			</Box>
+			<Snackbar
+				anchorOrigin={{ vertical: "top", horizontal: "center" }}
+				open={snackBarOpen}
+				autoHideDuration={1500}
+				onClose={() => setSnackBarOpen(false)}
+			>
+				Goals is completed
+			</Snackbar>
+		</ProtectRoutes>
+	);
 };
 
 export default FinanceGoals;
