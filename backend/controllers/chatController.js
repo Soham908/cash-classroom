b9818ba547e2d39514105360c75fba8efa0db8be
jaswant-rajput <jaswant.s@somaiya.edu>
2024-04-05 11:27:25 +0530

    some changes

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -4,12 +4,6 @@ dotenv.config({ path: "./../.env" });
 
 exports.getResponse = async (req, res) => {
 	try {
-		const prompt = `This model is trained to answer financial queries in a financial context.
-		 Please provide a financial query, and the model will respond accordingly, using concise text and only in text.
-		 
-		User Prompt : ${req.body.prompt}
-		Answer :
-		`;
 		const { GoogleGenerativeAI } = require("@google/generative-ai");
 		const genAI = new GoogleGenerativeAI(process.env.GEMINI_API);
 		const model = genAI.getGenerativeModel({ model: "gemini-pro" });
@@ -20,7 +14,7 @@ exports.getResponse = async (req, res) => {
 			},
 		});
 
-		const result = await chat.sendMessage(prompt);
+		const result = await chat.sendMessage(req.body.prompt);
 		const response = await result.response;
 		const text = response.text();
 
