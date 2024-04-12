7eed1d6071296dfe542a363dfa35e18b6f8747d8
jaswant-rajput <jaswant.s@somaiya.edu>
2024-04-12 09:30:33 +0530

    login sigup alerts added , lesssons completed checked logo

--- backend/models/courses.js
+++ backend/models/courses.js
@@ -2,48 +2,52 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const commentsSchema = new Schema({
-  userId :{
-    type : mongoose.Schema.Types.ObjectId,
-    required : true
-  },
-  comment : {
-    type : String,
-    required : true,
-    trim : true
-  }
-})
+	userId: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+	},
+	comment: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+});
 
 const courseSchema = new Schema({
-  img: {
-    type: String,
-    default: null,
-  },
-  title: {
-    unique: true,
-    type: String,
-    default: null,
-  },
-  desc: {
-    type: String,
-    default: null,
-  },
-  numChapters: {
-    type: Number,
-    default: null,
-  },
-  level: {
-    type: String,
-    default: null,
-  },
-  order: {
-    unique: true,
-    type: String,
-    default: null
-  },
-  comments : {
-    type : [commentsSchema],
-    default : []
-  }
+	img: {
+		type: String,
+		default: null,
+	},
+	title: {
+		unique: true,
+		type: String,
+		default: null,
+	},
+	desc: {
+		type: String,
+		default: null,
+	},
+	numChapters: {
+		type: Number,
+		default: null,
+	},
+	numQuizes: {
+		type: Number,
+		required: true,
+	},
+	level: {
+		type: String,
+		default: null,
+	},
+	order: {
+		unique: true,
+		type: String,
+		default: null,
+	},
+	comments: {
+		type: [commentsSchema],
+		default: [],
+	},
 });
 
 const Course = mongoose.model("Courses", courseSchema);
--- backend/models/users.js
+++ backend/models/users.js
@@ -1,60 +1,70 @@
 const mongoose = require("mongoose");
 
 const enrollCourseSchema = new mongoose.Schema({
-  course: {
-    type: String,
-  },
-  lessonsCompleted: {
-    type: Number,
-    default: 0,
-  },
-  totalLessons : {
-    type : Number,
-  },
-  is50MilestoneShown : {
-    type : Boolean,
-    default : false
-  },
-  is100MilestoneShown : {
-    type : Boolean,
-    default : false
-  }
+	course: {
+		type: String,
+	},
+	lessonsCompleted: {
+		type: Number,
+		default: 0,
+	},
+	totalLessons: {
+		type: Number,
+	},
+	marksObtainedTillNow: {
+		type: Number,
+	},
+	is50MilestoneShown: {
+		type: Boolean,
+		default: false,
+	},
+	is100MilestoneShown: {
+		type: Boolean,
+		default: false,
+	},
 });
 
 const lesson = new mongoose.Schema({
-  lessonName: {
-    type: String,
-  },
-  lessonId: {
-    type: String,
-  },
-  quizMarks: {
-    type: Number
-  },
-  totalQuestions: {
-    type: Number
-  }
+	courseName: {
+		type: String,
+		required: true,
+	},
+	lessonName: {
+		type: String,
+		required: true,
+	},
+	lessonId: {
+		type: String,
+	},
+	quizMarks: {
+		type: Number,
+		required: true,
+	},
+	totalQuestions: {
+		type: Number,
+		required: true,
+	},
 });
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    requied: [true, "Username is required"],
-  },
-  email: {
-    type: String,
-    trim: true,
-    required: [true, "User must have a email"],
-    unique: true,
-  },
-  password: {
-    type: String,
-    trim: true,
-    required: [true, "User must have a password"],
-  },
-  enrolledCourses: [enrollCourseSchema],
-  lessonsCompleted: [lesson],
+	name: {
+		type: String,
+		trim: true,
+		requied: [true, "Username is required"],
+	},
+	email: {
+		type: String,
+		trim: true,
+		required: [true, "User must have a email"],
+		unique: true,
+	},
+	password: {
+		type: String,
+		trim: true,
+		required: [true, "User must have a password"],
+	},
+	enrolledCourses: [enrollCourseSchema],
+	lessonsCompleted: [lesson],
 });
 
 const users = mongoose.model("Users", userSchema);
