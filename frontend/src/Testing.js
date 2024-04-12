4d580857941fae443c7d7488c0349cf2a178ce6d
jaswant-rajput <jaswant.s@somaiya.edu>
2024-04-12 23:03:05 +0530

    markdown editor testing done

--- frontend/src/Testing.js
+++ frontend/src/Testing.js
@@ -1,15 +1,92 @@
-import Exa from "exa-js";
+// import {
+// 	MDXEditor,
+// 	headingsPlugin,
+// 	listsPlugin,
+// 	markdownShortcutPlugin,
+// 	quotePlugin,
+// 	thematicBreakPlugin,
+// 	UndoRedo,
+// 	BoldItalicUnderlineToggles,
+// 	toolbarPlugin,
+// 	linkPlugin,
+// 	ListsToggle,
+// 	Separator,
+// } from "@mdxeditor/editor";
+
+// import "./testing.css";
+
+// const Testing = () => {
+// 	const markdown = `Hello [world](https://google.com/)`;
+// 	return (
+// 		<div style={{ padding: "100px" }}>
+// 			<MDXEditor
+// 				contentEditableClassName="mardownEditor"
+// 				style={{ padding: "30px" }}
+// 				plugins={[
+// 					headingsPlugin(),
+// 					listsPlugin(),
+// 					quotePlugin(),
+// 					markdownShortcutPlugin(),
+// 					thematicBreakPlugin(),
+// 					linkPlugin(),
+
+// 					toolbarPlugin({
+// 						toolbarContents: () => (
+// 							<nav className="toolbar">
+// 								<UndoRedo className="undoredo" />
+// 								<BoldItalicUnderlineToggles />
+// 								<ListsToggle />
+// 							</nav>
+// 						),
+// 					}),
+// 				]}
+// 				markdown={markdown}
+// 			/>
+// 		</div>
+// 	);
+// };
+
+// export default Testing;
+import "./testing.css";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 import { useState } from "react";
-import { Editor } from "react-draft-wysiwyg";
-import { EditorState, convertToRaw } from "draft-js";
-import "../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-// import draftToHtml from "draftjs-to-html";
-// import htmlToDraft from "html-to-draftjs";
+
+const modules = {
+	toolbar: [
+		[{ header: [1, 2, 3, 4, 5, 6, false] }],
+		[{ font: [] }],
+		[{ size: [] }],
+		["bold", "italic", "underline", "strike", "blockquote"],
+		[
+			{ list: "ordered" },
+			{ list: "bullet" },
+			{ indent: "-1" },
+			{ indent: "+1" },
+		],
+		["link", "image"],
+	],
+};
 
 const Testing = () => {
+	const [value, setValue] = useState("");
 	return (
-		<div style={{ display: "flex", padding: "100px" }}>
-			<Editor />;
+		<div className="containerTesting">
+			<div className="row">
+				<div className="editor">
+					<ReactQuill
+						theme="snow"
+						value={value}
+						onChange={setValue}
+						className="editor-input"
+						modules={modules}
+					/>
+				</div>
+				<div
+					className="preview"
+					dangerouslySetInnerHTML={{ __html: value }}
+				></div>
+			</div>
 		</div>
 	);
 };
