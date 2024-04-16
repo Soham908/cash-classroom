849097a60fa9d9eab32baaa19f36bc0ed571f081
Soham908 <sohampalekar908@gmail.com>
2024-04-16 08:47:53 +0530

    certificate task completed

--- frontend/src/components/Certificate.js
+++ frontend/src/components/Certificate.js
@@ -1,36 +1,38 @@
-import { Typography } from "@mui/material";
+import { Document, Page, Text, Image } from "@react-pdf/renderer";
+import certImage from "./courseCompletionCertificate2.png"
 import { useAuthStore } from "../store/store";
 
+
 const Certificate = () => {
+  const userAuthStateData = useAuthStore.getState().user
 
-    const userAuthStateData = useAuthStore.getState().user
-
-    console.log(userAuthStateData);
+  console.log(userAuthStateData);
   return (
-    <>
-      <img src="/courseCompletionCertificate.png" height={600} />
-      <Typography
-        variant="h4"
-        sx={{
-          position: "absolute",
-          bottom: "7%",
-          left: "22%",
+    <Document>
+    <Page size={{ width: 650, height: 450 }}>
+      <Image src={certImage} style={{ width: '100%', height: '100%' }} />
+      <Text 
+        style={{
+            position:"absolute",
+            top:"42%",
+            left:"38%",fontSize: 20, fontWeight: 'bold'
         }}
       >
         {userAuthStateData.data.name}
-      </Typography>
-      <Typography
-        variant="h6"
-        sx={{
-          position: "absolute",
-          bottom: "-5.5%",
-          left: "12.5%",
+      </Text>
+
+      <Text 
+        style={{
+            position:"absolute",
+            bottom:"40%",
+            left:"38%",fontSize: 16, fontWeight: 'bold'
         }}
       >
-        Technical Analysis
-      </Typography>
-    </>
+        Option Trading course.
+      </Text>
+    </Page>
+  </Document>
   );
 };
 
-export default Certificate
+export default Certificate;
